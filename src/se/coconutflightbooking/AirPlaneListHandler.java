c73f9cbc4d213718819678f465c538bd1fde9788
Unknown <niklas.a.svensson@gmail.com>
2017-11-22 16:54:36 +0100

    Korrigerat en länkning av en Exception i AirPlane efter tidigare flytt av Exception

--- src/se/coconutflightbooking/AirPlaneListHandler.java
+++ src/se/coconutflightbooking/AirPlaneListHandler.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import se.coconutflightbooking.exceptions.AirPlaneNotFoundException;
+
 public class AirPlaneListHandler {
 	HashMap<String, AirPlane> ourAirPlaneList = new HashMap<String, AirPlane>();
 
